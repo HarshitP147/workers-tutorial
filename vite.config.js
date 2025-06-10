95d59edaea07163c7bb80d20d2630a23363879eb
Harshit147 <95279636+HarshitP147@users.noreply.github.com>
2025-06-10 16:38:03 +0000

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
-    // config options
-    server: {
-        allowedHosts: ['https://fpnhfp-5173.csb.app']
-    }
-}
+  // config options
+  server: {
+    allowedHosts: true,
+  },
+};
